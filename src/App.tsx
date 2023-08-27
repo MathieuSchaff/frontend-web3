2942ccda541d5aeb9b3f14bca2e58d0b9ed82f4a
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-27 22:31:20 +0200

    deleted all the logic in the app component and breaking it into multiple components and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-// import { useEffect, useState } from 'react'
 import './App.global.css'
 import styles from './App.module.css'
-// import detectEthereumProvider from '@metamask/detect-provider';
-// import { formatBalance, formatChainAsNum } from './utils/crypto';
 import { Navigation } from './components/Navigation'
 import { Display } from './components/Display'
 import { MetaMaskError } from './components/MetaMaskError'
-
-// const initialStateAccounts = { accounts: [], balance: "", chainId: "" }
+import { MetaMaskContextProvider } from './hooks/useMetaMask'
 function App() {
   return (
     <>
-      <div className={styles.appContainer}>
-        <Navigation />
-        <Display />
-        <MetaMaskError />
-      </div>
+      <MetaMaskContextProvider>
+        <div className={styles.appContainer}>
+          <Navigation />
+          <Display />
+          <MetaMaskError />
+        </div>
+      </MetaMaskContextProvider>
     </>
   )
 }
