6a8dec4ef85c2ad2005db36e2404b1185bc75006
MathieuSchaff <schaff.mathieu93@gmail.com>
2023-08-29 00:42:25 +0200

    fund function added

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,13 +1,29 @@
-
+import { string } from 'valibot'; // 0.6 kB
 import { useRef } from 'react'
 import { useMetaMask } from '../../hooks/useMetaMask'
-import { formatChainAsNum } from '../../utils/crypto.ts'
+import { formatBalance, formatChainAsNum } from '../../utils/crypto.ts'
 import styles from './Display.module.css'
-import { ethers, BrowserProvider, parseUnits } from 'ethers'
+import { ethers } from 'ethers'
+import { abi } from '../../utils/constants'
+const FundAmountSchema = string([
+  (input) => {
+    if (input.length == 0) {
+      return {
+        issue: {
+          error: true,
+          validation: 'custom',
+          message: 'Invalid length',
+          input,
+        },
+      };
+    }
+    return { output: input };
+  },
+]);
 export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 export const Display = () => {
-  const amountRefInput = useRef(null)
+  const amountRefInput = useRef<HTMLInputElement>(null)
   const { wallet } = useMetaMask()
 
   async function getBalance() {
@@ -16,17 +32,41 @@ export const Display = () => {
       console.log(provider)
       try {
         const balance = await provider.getBalance(contractAddress)
+        const balanceFormated = formatBalance(balance)
         console.log(balance)
+        console.log(balanceFormated)
       } catch (error) {
         console.log(error)
       }
     }
   }
-  // async function fund() {
-  //   const amountToFund = amountRefInput.current.value 
-  //   console.log(amountToFund)
-  //   if (typeof window.ethereum !== "undefined") {
-  //     const provider = new ethers.BrowserProvider(window.ethereum)
+  async function fund() {
+    let signer = null;
+    const amountToFund = amountRefInput.current?.value
+    if (typeof amountToFund !== "string") {
+      return
+    }
+    if (FundAmountSchema._parse(amountToFund).issues) {
+      console.log(FundAmountSchema._parse(amountToFund))
+      return
+    }
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.BrowserProvider(window.ethereum)
+      signer = await provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer)
+      try {
+        const transactionResponse = await contract.fund({
+          value: ethers.parseEther(amountToFund),
+        })
+        console.log(transactionResponse)
+        const receipt = await transactionResponse.wait()
+        console.log(receipt)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
 
 
 
@@ -43,7 +83,7 @@ export const Display = () => {
             <button id="withdrawButton">Withdraw</button>
             <label htmlFor="ethAmount">ETH Amount</label>
             <input id="ethAmount" placeholder="0.1" ref={amountRefInput} />
-            <button type="button" id="fundButton" > Fund </button>
+            <button onClick={fund} type="button" id="fundButton" > Fund </button>
           </section>
         </>
       }
